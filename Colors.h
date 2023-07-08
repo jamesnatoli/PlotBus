433fd81007dc8ff20e12464df910d73f1753dc45
James Natoli <jamesnatoli@natolimacbook.dyndns.cern.ch>
2023-07-08 12:32:37 +0200

    more options for tuned samples

--- Colors.h
+++ Colors.h
@@ -2,8 +2,10 @@
 #define COLORS_H
 
 #include "TColor.h"
+#include "PlotBus.cc"
+// Maybe add a thing such that if the color isn't found it gives is a random one?
 
-std::map<std::string, int> colors{
+std::map<std::string, int> PlotBus::colors{
   {"DY", 865},
     {"ST", 38},
       {"TT", 592},
@@ -17,22 +19,39 @@ std::map<std::string, int> colors{
 		    {"QCD Est Bins", kOrange},
 		      {"QCD", 606},
 			{"W3pi", 2},
-			  {"ST_Top",kBlack},
-			    {"ST_TopUp", kBlue},
-			      {"ST_TopDown", kGreen},
-				{"ST_AntiTop", kBlack},
-				  {"ST_AntiTopUp", kBlue},
-				    {"ST_AntiTopDown", kGreen},
-				      {"MultiBoson", kOrange},
-					{"Top", kGreen},
-					  {"", kGray}
+			  {"MultiBoson", kOrange},
+			    {"Top", kGreen},
+			      {"Others", kGreen},
+				{"ST_Top",kBlack},
+				  {"ST_AntiTop", kBlack},
+				    {"ST Nominal", kBlack},
+				      {"TTo2L2Nu",kBlack},
+					{"TTToSemiLeptonic", kBlack},
+					  {"TTTohadronic", kBlack},
+					    { "TT Nominal", kBlack},
+					    // Tuned Samples
+					      {"ST Up", kBlue},
+						{"ST Down", kGreen},
+						  {"ST_TopUp", kBlue},
+						    {"ST_TopDown", kGreen},
+						      {"ST_AntiTopUp", kRed},
+							{"ST_AntiTopDown", kOrange},
+							  {"TT Up", kBlue},
+							    {"TT Down", kGreen},
+							      {"TTTo2L2Nu_Up", kBlue},
+								{"TTTo2L2Nu_Down", kGreen},
+								  {"TTToSemiLeptnic_Up", kBlue},
+								    {"TTToSemiLeptonic_Down", kGreen},
+								      {"TTToHadronic_Up", kBlue},
+									{"TTTohadronic_Down", kGreen},
+									  {"", kGray}
 };
 
 // strip the region if necessary
 int getColors( std::string proc) {
   if ( proc.find("_") != std::string::npos)
-    return colors[proc];
+    return PlotBus::colors[proc];
   else
-    return colors[ proc.substr(0, proc.find("_"))];
+    return PlotBus::colors[ proc.substr(0, proc.find("_"))];
 }
 #endif // COLORS_H
